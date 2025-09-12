5007a35362b824f0e110656ba127a80f888e1365
roxibelandreinacastillopetit08-ops <roxibelandreinacastillopetit08@gmail.com>
2025-09-12 17:03:51 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+mi_diccionario = {
+    "nombre": "roxibel",
+    "apellido": "castillo",
+    "color": "lila",
+}
+
+
+for clave, valor in mi_diccionario.items():
+    print(f"\nclave: {clave}, valor: {valor}")
