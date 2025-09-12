668e809ef750aefe7e2b76b818158b89a2b84387
roxibelandreinacastillopetit08-ops <roxibelandreinacastillopetit08@gmail.com>
2025-09-12 17:03:07 -0400

    Add files via upload

---
+++ 1tarea.py
@@ -0,0 +1,18 @@
+contador = 0
+
+while contador < 11:
+    print(f"contador: {contador}")
+    contador += 1
+
+
+for numero in range (11) :
+        print(f" numero actual:{numero}")
+
+      
+        
+
+
+
+
+
+      
